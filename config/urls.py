3ef449c1ec0a9a2e91a1f49e9bbf41d8eb9b2195
Esteban Flores <quitosoft@gmail.com>
2025-08-16 19:50:31 -0300

    urls

--- config/urls.py
+++ config/urls.py
@@ -6,10 +6,10 @@ from avisos.views import logout_then_login
 from avisos.forms import BootstrapAuthenticationForm
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("accounts/login/", auth_views.LoginView.as_view(template_name="auth/login.html",authentication_form=BootstrapAuthenticationForm), name="login"),
-    path("accounts/logout/", logout_then_login, name="logout"),
-    path("accounts/signup/", SignupView.as_view(), name="signup"),
+    path("interno/", admin.site.urls),
+    path("a/login/", auth_views.LoginView.as_view(template_name="auth/login.html",authentication_form=BootstrapAuthenticationForm), name="login"),
+    path("a/logout/", logout_then_login, name="logout"),
+    path("a/signup/", SignupView.as_view(), name="signup"),
     path("", HomeView.as_view(), name="home"),
     path("", include("avisos.urls")),
 ]
